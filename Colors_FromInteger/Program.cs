b1c8690f20b29baf443ee4295249cc7b8eed1c98
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2023-01-26 09:59:21 -0500

    Add Colors_FromInteger

---
+++ Colors_FromInteger/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using Windows.UI;
+
+namespace Benchmark;
+
+class Program {
+	static void Main ()
+	{
+		BenchmarkRunner.Run<Colors_FromInteger>();
+	}
+}
+
+public class Colors_FromInteger
+{
+	public static IEnumerable<int> GetAllInt32Values()
+	{
+		for (int i = -100; i <= 100; i++)
+		// for (int i = int.MinValue; i <= int.MaxValue; i++)
+		{
+			yield return i;
+		}
+	}
+
+	// [ArgumentsSource(nameof(GetAllInt32Values))]
+	[Benchmark]
+	public void FromInteger()
+	{
+		for (int i = int.MinValue; i <= int.MaxValue; i++)
+		{
+			Colors.FromInteger(i);
+		}
+	}
+}
