718ce19316b25c181eecd6cba3ea7d1d2997006b
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2023-01-30 19:59:31 -0500

    Colors_FromARGB: loop long enough to get good results

--- Colors_FromARGB/Program.cs
+++ Colors_FromARGB/Program.cs
@@ -1,4 +1,4 @@
-﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 using Windows.UI;
@@ -20,19 +20,49 @@ public class Colors_FromARGB
 
 	static Colors_FromARGB()
 	{
-		list.Add(null);
+		// list.Add(null); fails with NRE on uno/master
 		list.Add("");
 		list.Add(" ");
-		for (int i = 0; i < 16; i++) {
-			char c = hex[i];
-			list.Add($"{c}{c}{c}");					// RGB
-			list.Add($"#{c}{c}{c}");				// #RGB
-			list.Add($"{c}{c}{c}{c}");				// ARGB
-			list.Add($"#{c}{c}{c}{c}");				// #ARGB
-			list.Add($"{c}{c}{c}{c}{c}{c}");		// RRGGBB
-			list.Add($"#{c}{c}{c}{c}{c}{c}");		// #RRGGBB
-			list.Add($"{c}{c}{c}{c}{c}{c}{c}{c}");	// AARRGGBB
-			list.Add($"#{c}{c}{c}{c}{c}{c}{c}{c}");	// #AARRGGBB	
+		for (int h = 0; h < 16; h++)
+		{
+			char a = hex[h];
+			for (int i = 0; i < 16; i++)
+			{
+				char r = hex[i];
+				for (int j = 0; j < 16; j++)
+				{
+					char g = hex[j];
+					for (int k = 0; k < 16; k++)
+					{
+						char b = hex[k];
+						list.Add($"{r}{g}{b}");		// RGB
+						list.Add($"#{r}{g}{b}");	// #RGB
+						list.Add($"{a}{r}{g}{b}");	// ARGB
+						list.Add($"#{a}{r}{g}{b}");	// #ARGB
+
+						for (int l = 0; l < 16; l+=4)
+						{
+							char a2 = hex[l];
+							for (int m = 0; m < 16; m+=4)
+							{
+								char r2 = hex[m];
+								for (int n = 0; n < 16; n+=4)
+								{
+									char g2 = hex[n];
+									for (int o = 0; o < 16; o+=4)
+									{
+										char b2 = hex[o];
+										list.Add($"{r}{r2}{g}{g2}{b}{b2}");			// RRGGBB
+										list.Add($"#{r}{r2}{g}{g2}{b}{b2}");		// #RRGGBB
+										list.Add($"{a}{a2}{r}{r2}{g}{g2}{b}{b2}");	// AARRGGBB
+										list.Add($"#{a}{a2}{r}{r2}{g}{g2}{b}{b2}");	// #AARRGGBB
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
